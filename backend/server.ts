68729211dea57e036b282f6f1343454ad87e9bfd
מרים <miriam@example.com>
2026-05-13 11:46:45 +0300

    feat: initialize express server and integrate global middleware and routes
    Server: Configured Express and entry point. Database: Integrated MongoDB connection. Middleware: Added CORS and global error handling. Routes: Connected all resource routers.

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,28 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import { connectDB } from './Utils/ConnectDB';
 import { errorHandler } from './middleware/errorHandling';
+import categoryRoutes from './routes/CategoryRoute';
+import subCategoryRoutes from './routes/SubCategoryRoute';
+import userRoutes from './routes/UserRoute';
+import promptRoutes from './routes/PrompyRoute';
+
 const app = express();
+
+// Middlewares
+app.use(cors()); // מאפשר ל-Frontend לגשת לשרת
 app.use(express.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/subcategories', subCategoryRoutes);
+app.use('/api/prompts', promptRoutes);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
+
 const startServer = async () => {
   try {
     await connectDB();
@@ -18,7 +36,5 @@ const startServer = async () => {
     process.exit(1);
   }
 };
-
-app.use(errorHandler);
 
 startServer();
